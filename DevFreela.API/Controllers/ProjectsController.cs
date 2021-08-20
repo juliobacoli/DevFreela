c04940e13c96f9cc6c2b7a90f91d7aad14128c4a
Julio Bacoli <juliosantanabacoli@gmail.com>
2021-08-20 11:56:54 -0300

    Inicio endpoints Criação de models

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,43 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        //api/projects?query=net core
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if(updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
