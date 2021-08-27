441e196293947121cb506e3e5b55a4306f0abfdc
Julio Bacoli <juliosantanabacoli@gmail.com>
2021-08-27 17:40:48 -0300

    Implementação do padrão Option e configuração de ambiente

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -1,3 +1,4 @@
+using DevFreela.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,10 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace DevFreela.API
 {
@@ -26,6 +24,7 @@ namespace DevFreela.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<OpeningTimeOption>(Configuration.GetSection("OpeningTime"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
