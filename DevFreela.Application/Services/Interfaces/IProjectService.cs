4172e20c92e293133df1094419296f49218f06c7
Julio Bacoli <juliosantanabacoli@gmail.com>
2021-10-08 18:12:20 -0300

    Implementações em UsersController, interface de User, service de User, input e viewModel de User

--- DevFreela.Application/Services/Interfaces/IProjectService.cs
+++ DevFreela.Application/Services/Interfaces/IProjectService.cs
@@ -1,10 +1,6 @@
 ﻿using DevFreela.Application.InputModels;
 using DevFreela.Application.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevFreela.Application.Services.Interfaces
 {
