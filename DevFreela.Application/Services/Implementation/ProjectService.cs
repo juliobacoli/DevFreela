cdd1a03377eddb7e5f326ee61a53880f9196ae4e
Julio Bacoli <juliosantanabacoli@gmail.com>
2022-01-20 15:21:54 -0300

    adicionando propriedades

--- DevFreela.Application/Services/Implementation/ProjectService.cs
+++ DevFreela.Application/Services/Implementation/ProjectService.cs
@@ -3,6 +3,7 @@ using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entites;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -63,7 +64,10 @@ namespace DevFreela.Application.Services.Implementation
 
         public ProjectDetailsViewModel GetById(int id)
         {
-            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+            var project = _dbContext.Projects
+                .Include(p => p.Client)
+                .Include(p => p.Freelancer)
+                .SingleOrDefault(p => p.Id == id);
 
             if (project == null) return null;
 
@@ -73,7 +77,10 @@ namespace DevFreela.Application.Services.Implementation
                 project.Description,
                 project.TotalCost,
                 project.StartedAt,
-                project.FinishedAt
+                project.FinishedAt,
+                project.Client.FullName,
+                project.Freelancer.FullName
+                
                 );
 
             return projectDetailsViewModel;
