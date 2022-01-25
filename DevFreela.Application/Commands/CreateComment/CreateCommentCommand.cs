bf756138830e1490eb031f018e4c4432938a8ad9
Julio Bacoli <juliosantanabacoli@gmail.com>
2022-01-25 00:16:31 -0300

    implementando commands, inserindo controller skills, refatorando services e interface

---
+++ DevFreela.Application/Commands/CreateComment/CreateCommentCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.CreateComment
+{
+    public class CreateCommentCommand : IRequest<Unit> //UNIT É O TIPO SEM RETORNO (VOID) DO MEDIATR
+    {
+        public string Content { get; set; }
+        public int IdProject { get; set; }
+        public int IdUser { get; set; }
+    }
+}
