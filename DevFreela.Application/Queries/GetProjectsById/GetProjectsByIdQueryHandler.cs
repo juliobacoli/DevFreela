d275a1c79f41c8262c178c7b98839e1abece32a9
Julio Bacoli <juliosantanabacoli@gmail.com>
2022-06-28 18:11:42 -0300

    refatoração querie handler

--- DevFreela.Application/Queries/GetProjectsById/GetProjectsByIdQueryHandler.cs
+++ DevFreela.Application/Queries/GetProjectsById/GetProjectsByIdQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.Repositories;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -13,19 +14,16 @@ namespace DevFreela.Application.Queries.GetProjectsById
 {
     public class GetProjectsByIdQueryHandler : IRequestHandler<GetProjectsByIdQuery, ProjectDetailsViewModel>
     {
-        private readonly DevFreelaDbContext _dbContext;
+        private readonly IProjectRepository _projectRepository;
 
-        public GetProjectsByIdQueryHandler(DevFreelaDbContext dbContext)
+        public GetProjectsByIdQueryHandler(IProjectRepository projectRepository)
         {
-            _dbContext = dbContext;
+            _projectRepository = _projectRepository;
         }
 
         public async Task<ProjectDetailsViewModel> Handle(GetProjectsByIdQuery request, CancellationToken cancellationToken)
         {
-            var project = await _dbContext.Projects
-                .Include(p => p.Client)
-                .Include(p => p.Freelancer)
-                .SingleOrDefaultAsync(p => p.Id == request.Id);
+            var project = await _projectRepository.GetDetailsByIdAsync(request.Id);
 
             if (project == null) return null;
 
