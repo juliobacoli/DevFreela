8988ac5b2b267816e0a89df560e1e5ed548cb132
Júlio Bacoli <juliosantanabacoli@gmail.com>
2022-07-05 19:00:10 -0300

    GIT TESTE

--- DevFreela.Core/DTOs/SkillDTO.cs
+++ DevFreela.Core/DTOs/SkillDTO.cs
@@ -9,6 +9,6 @@ namespace DevFreela.Core.DTOs
     public class SkillDTO
     {
         public int Id { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } 
     }
 }
