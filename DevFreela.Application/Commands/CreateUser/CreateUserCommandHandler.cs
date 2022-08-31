332f0641fcc53ef76063ed973beb1fc1720fa210
Julio Bacoli <juliosantanabacoli@gmail.com>
2022-08-31 20:10:04 -0300

    salvando a senha com encriptação SHA256

--- DevFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
+++ DevFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DevFreela.Core.Entites;
+using DevFreela.Core.Services;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 using System;
@@ -13,14 +14,17 @@ namespace DevFreela.Application.Commands.CreateUser
     public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
     {
         private readonly DevFreelaDbContext _dbContext;
+        private readonly IAuthService _authService;
 
-        public CreateUserCommandHandler(DevFreelaDbContext dbContext)
+        public CreateUserCommandHandler(DevFreelaDbContext dbContext, IAuthService authService)
         {
             _dbContext = dbContext;
+            _authService = authService;
         }
 
         public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
+            var passwordHas = _authService.ComputeSha256Hash(request.Password);
             var user = new User(request.FullName, request.Email, request.BirthDate, request.Password, request.Role);
 
             await _dbContext.Users.AddAsync(user);
--- DevFreela.Infrastructure/Auth/AuthService.cs
+++ DevFreela.Infrastructure/Auth/AuthService.cs
@@ -4,7 +4,9 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.Intrinsics.Arm;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace DevFreela.Infrastructure.Auth
@@ -16,6 +18,24 @@ namespace DevFreela.Infrastructure.Auth
         public AuthService(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+
+        public string ComputeSha256Hash(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                //ComputerHash - retorna byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                //Converte byte array para string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2")); //x2 faz com que seja convertido em representa
+                }
+
+                return builder.ToString();
+            }
         }
 
         public string GenerateJwtToken(string email, string role)
