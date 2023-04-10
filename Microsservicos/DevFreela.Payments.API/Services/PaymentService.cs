44bf95106e743907cf64335e3fd9dfae7706bf52
Julio Bacoli <juliosantanabacoli@gmail.com>
2023-04-10 19:11:40 -0300

    criando microsservico de pagamento

---
+++ Microsservicos/DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Payments.API.Models;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            //Apenas para teste. Não vou implementar aqui.
+            return Task.FromResult(true);
+        }
+    }
+}
