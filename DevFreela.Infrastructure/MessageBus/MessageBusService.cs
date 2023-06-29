296fb2e034807898aa8e210197503435bf315879
Julio Santana <juliosantanabacoli@gmail.com>
2023-06-29 01:05:07 -0300

    Implementando RabbitMQ

---
+++ DevFreela.Infrastructure/MessageBus/MessageBusService.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Core.Services;
+
+namespace DevFreela.Infrastructure.MessageBus
+{
+    public class MessageBusService : IMessageBusService
+    {
+        public MessageBusService()
+        {
+
+        }
+
+        public void Publish(string queue, byte[] message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
